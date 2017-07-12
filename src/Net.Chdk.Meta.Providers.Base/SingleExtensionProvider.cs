df8dd250c1bb32a4f621aff952a05d7c5582ef28
Dmitry Shechtman <damnian@gmail.com>
2017-07-13 01:59:10 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.Base/SingleExtensionProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Net.Chdk.Meta.Providers
+{
+    public abstract class SingleExtensionProvider<TInnerProvider>
+        where TInnerProvider : IExtensionProvider
+    {
+        private IEnumerable<TInnerProvider> InnerProviders { get; }
+
+        protected SingleExtensionProvider(IEnumerable<TInnerProvider> innerProviders)
+        {
+            InnerProviders = innerProviders;
+        }
+
+        protected TInnerProvider GetInnerProvider(string path, out string ext)
+        {
+            var x = ext = Path.GetExtension(path);
+            return InnerProviders.SingleOrDefault(p => x.Equals(p.Extension, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
